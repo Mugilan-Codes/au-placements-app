8db3fac9c3f87e8cb7a56d09e3a96d142b653e33
Mugilan <mugilancodes@gmail.com>
2020-12-18 23:51:20 +0530

    Form Button

---
+++ App/components/FormButton.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+
+import {Mixins} from '../styles';
+
+const FormButton = ({buttonTitle, ...rest}) => (
+  <TouchableOpacity style={styles.buttonContainer} {...rest}>
+    <Text style={styles.buttonText}>{buttonTitle}</Text>
+  </TouchableOpacity>
+);
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    marginTop: 10,
+    width: Mixins.WINDOW_WIDTH / 2,
+    height: Mixins.WINDOW_HEIGHT / 15,
+    backgroundColor: '#6646ee',
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 8,
+  },
+  buttonText: {
+    fontSize: 28,
+    color: '#ffffff',
+  },
+});
+
+export default FormButton;

eb61b7f897f91cd991d14955fcd9666e0325317c
Mugilan <mugilancodes@gmail.com>
2020-12-19 21:26:30 +0530

    API_URL constants

---
+++ App/api/constants.js
@@ -0,0 +1,7 @@
+//! Test URL .. Change after Deploying
+const API_URL = 'http://localhost:5000/api';
+
+export const Auth = {
+  REGISTER: `${API_URL}/student/register`,
+  LOGIN: `${API_URL}/student/login`,
+};

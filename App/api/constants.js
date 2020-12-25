97647e645f1a253396f41e9ffec15bb53744bfb8
Mugilan <mugilancodes@gmail.com>
2020-12-25 17:36:48 +0530

    Added some more routes

--- App/api/constants.js
+++ App/api/constants.js
@@ -4,4 +4,5 @@ const API_URL = 'http://localhost:5000/api';
 export const Auth = {
   REGISTER: `${API_URL}/student/register`,
   LOGIN: `${API_URL}/student/login`,
+  GET: `${API_URL}/student`,
 };

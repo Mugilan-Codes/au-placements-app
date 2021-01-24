480710e3a0c067b4969232de180ea2c505a31b5d
Mugilan <mugilancodes@gmail.com>
2021-01-24 15:22:56 +0530

    Update Profile todo

--- App/screens/profile/index.js
+++ App/screens/profile/index.js
@@ -1,10 +1,12 @@
 import React, {useEffect} from 'react';
-import {SafeAreaView, Text, TouchableHighlight} from 'react-native';
+import {SafeAreaView, Text, TouchableHighlight, StyleSheet} from 'react-native';
 
 import {useAuth} from '../../state/providers/auth/provider';
 
 const ProfleScreen = ({navigation}) => {
   const {state, loadUser, logout} = useAuth();
+
+  // todo: Add Buttons to Add/Edit Marks and Education
 
   useEffect(() => {
     loadUser();
@@ -15,7 +17,7 @@ const ProfleScreen = ({navigation}) => {
   };
 
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <SafeAreaView style={styles.container}>
       <Text>ProfleScreen</Text>
       <Text>{state?.user?.email}</Text>
       <TouchableHighlight onPress={onLogout}>
@@ -24,5 +26,13 @@ const ProfleScreen = ({navigation}) => {
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default ProfleScreen;

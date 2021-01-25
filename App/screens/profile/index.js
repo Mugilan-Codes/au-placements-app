3339836486001ebefe5ea7cb00e5a54df146174c
Mugilan <mugilancodes@gmail.com>
2021-01-25 18:04:48 +0530

    Add Buttons to add marks and education

--- App/screens/profile/index.js
+++ App/screens/profile/index.js
@@ -1,5 +1,11 @@
 import React, {useEffect} from 'react';
-import {SafeAreaView, Text, TouchableHighlight, StyleSheet} from 'react-native';
+import {
+  SafeAreaView,
+  Text,
+  TouchableHighlight,
+  StyleSheet,
+  Alert,
+} from 'react-native';
 
 import {useAuth} from '../../state/providers/auth/provider';
 
@@ -19,7 +25,20 @@ const ProfleScreen = ({navigation}) => {
   return (
     <SafeAreaView style={styles.container}>
       <Text>ProfleScreen</Text>
-      <Text>{state?.user?.email}</Text>
+      <Text>{state?.user?.name}</Text>
+
+      {!state.user?.mark ? (
+        <TouchableHighlight onPress={() => Alert.alert('Add Marks')}>
+          <Text>Add Marks</Text>
+        </TouchableHighlight>
+      ) : null}
+
+      {!state.user?.education ? (
+        <TouchableHighlight onPress={() => Alert.alert('Add Education')}>
+          <Text>Add Education</Text>
+        </TouchableHighlight>
+      ) : null}
+
       <TouchableHighlight onPress={onLogout}>
         <Text>LOGOUT</Text>
       </TouchableHighlight>

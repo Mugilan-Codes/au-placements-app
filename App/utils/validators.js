704806406be5a3119b87cb200d51e43c57ae8992
Mugilan <mugilancodes@gmail.com>
2021-01-26 18:35:13 +0530

    Updated Comments

--- App/utils/validators.js
+++ App/utils/validators.js
@@ -1,3 +1,5 @@
+// https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+// https://stackoverflow.com/a/46181/12381908
 // const EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
 const EMAIL_REGEX = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 

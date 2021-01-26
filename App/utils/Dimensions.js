ce2088ad8ac6e968ea236f95862cb2c7a2d490b7
Mugilan <mugilancodes@gmail.com>
2021-01-26 18:21:06 +0530

    Update Dimensions Utils

--- App/utils/Dimensions.js
+++ App/utils/Dimensions.js
@@ -1,8 +1,11 @@
+// https://reactnative.dev/docs/usewindowdimensions
 import {Dimensions} from 'react-native';
 
 const Window = {
   WINDOW_WIDTH: Dimensions.get('window').width,
   WINDOW_HEIGHT: Dimensions.get('window').height,
+  FONT_SCALE: Dimensions.get('window').fontScale,
+  SCALE: Dimensions.get('window').scale,
 };
 
 export default Window;

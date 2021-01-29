65f2c2c9ccbec12d4a93e3269bd24bf3b8dd391b
Mugilan <mugilancodes@gmail.com>
2021-01-29 18:00:40 +0530

    Add hook-rules eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  plugins: ['react-hooks'], // https://reactjs.org/docs/hooks-rules.html
+  rules: {
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+  },
 };

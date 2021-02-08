b05c804ca2720e889bdfd751942cd8d273502665
Mugilan <mugilancodes@gmail.com>
2021-02-08 18:44:54 +0530

    Logging the ColorScheme of the device

--- App/index.js
+++ App/index.js
@@ -1,10 +1,13 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import {useColorScheme} from 'react-native';
 
 import {AppNavigator} from './navigations';
 import {StateProvider} from './state';
 
 const App = () => {
+  const colorScheme = useColorScheme();
+  console.log({colorScheme});
   return (
     <StateProvider>
       <AppNavigator />

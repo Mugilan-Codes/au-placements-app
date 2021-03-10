8975f402a878dcd6a1bcc391f8a1bb9b8c6f8701
Mugilan <mugilancodes@gmail.com>
2021-03-10 16:27:24 +0530

    Loading action in one listing

--- App/screens/modal/index.js
+++ App/screens/modal/index.js
@@ -1,34 +1,45 @@
 import React, {useEffect, useState} from 'react';
 import {Text, View, Button, StyleSheet} from 'react-native';
+
 import {Student} from '../../api';
+import {Loading} from '../../components';
 
 // TODO: Build this to get ID and call API to fetch listings.
 const ModalScreen = ({route, navigation}) => {
   const {id} = route.params;
-  const [listing, setListing] = useState({});
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     getListing(id);
   }, [id]);
 
   const getListing = async (list_id) => {
+    setLoading(true);
     const {data} = await Student.getOneListing(list_id);
     setListing(data);
+    setLoading(false);
   };
 
   const dismissList = () => {
-    setListing({});
+    setListing(null);
     navigation.goBack();
   };
 
   console.log(listing);
   return (
-    <View style={styles.modalView}>
-      <Text style={styles.titleStyle}>
-        This is a modal! for {listing.title}
-      </Text>
-      <Button onPress={dismissList} title="Dismiss" />
-    </View>
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <View style={styles.modalView}>
+          <Text style={styles.titleStyle}>
+            This is a modal! for {listing.title}
+          </Text>
+          <Button onPress={dismissList} title="Dismiss" />
+        </View>
+      )}
+    </>
   );
 };
 

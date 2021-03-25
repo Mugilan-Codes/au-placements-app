7718195c8f88b50620b7f4c6851e19bc2c2bb94b
Mugilan <mugilancodes@gmail.com>
2021-03-25 21:55:32 +0530

    Removed useColorSceme from App/index.js

--- App/index.js
+++ App/index.js
@@ -1,15 +1,10 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {useColorScheme} from 'react-native';
 
 import {AppNavigator} from './navigations';
 import {StateProvider} from './state';
 
 const App = () => {
-  // TODO: move the colorScheme to change the Theme of the app. Set the Theme to system settigs
-  const colorScheme = useColorScheme();
-  console.log({colorScheme});
-
   return (
     <StateProvider>
       <AppNavigator />

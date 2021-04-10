e202c383fc1d054cb2ba6c1d07e441f9dbde4fbe
Mugilan <mugilancodes@gmail.com>
2021-04-10 19:54:44 +0530

    Add Date Utility Function

---
+++ App/utils/date.js
@@ -0,0 +1,7 @@
+import dayjs from 'dayjs';
+import localIn from 'dayjs/locale/en-in';
+
+const _currentTimeDefaultFormat = 'hh:mm:ss A --> ddd DD/MM/YYYY'; // 11:17:09 AM --> Sat 10/04/2021
+export const getCurrentTime = ({format = _currentTimeDefaultFormat} = {}) => {
+  return dayjs().locale(localIn).format(format);
+};
--- App/utils/index.js
+++ App/utils/index.js
@@ -1,4 +1,8 @@
 export {default as Dimensions} from './Dimensions';
 export {default as setAuthToken} from './setAuthToken';
 export {default as storage} from './asyncStorage';
+
 export {validators} from './validators';
+
+import * as Date from './date';
+export {Date};

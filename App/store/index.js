20bf2a93d7987d4d45d66af9a5c7ec5bd7a400b5
Mugilan <mugilancodes@gmail.com>
2021-11-23 22:40:27 +0530

    redux persist :sparkles:

--- App/store/index.js
+++ App/store/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
 
-import {store} from './store';
+import {store, persistor} from './store';
 
 const StoreProvider = ({children}) => {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 };
 
 export default StoreProvider;
--- App/store/store.js
+++ App/store/store.js
@@ -1,9 +1,39 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {configureStore, combineReducers} from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import authSlice from './slices/authSlice';
 
-export const store = configureStore({
-  reducer: {
-    auth: authSlice,
-  },
+const rootReducer = combineReducers({
+  auth: authSlice,
 });
+
+const persistConfig = {
+  key: 'root',
+  stateReconciler: autoMergeLevel2,
+  storage: AsyncStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);

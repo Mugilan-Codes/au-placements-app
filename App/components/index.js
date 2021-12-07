ddc821f8df7f463bed3fc5a14bb8f0998d67ba9d
Mugilan <mugilancodes@gmail.com>
2021-12-07 23:02:40 +0530

    add common style components :lipstick:

--- App/components/index.js
+++ App/components/index.js
@@ -2,6 +2,8 @@ export {default as FormButton} from './common/FormButton';
 export {default as FormInput} from './common/FormInput';
 export {default as Loading} from './common/Loading';
 export {default as ScreenHeader} from './common/ScreenHeader';
+export {default as SizedBox} from './common/SizedBox';
+export * from './common/Styled';
 
 export {default as List} from './listing/List';
 export {default as ListSeparator} from './listing/ListSeparator';

c948caace485d174c42653f9eb5f5bf619cfb6bc
Mugilan <mugilancodes@gmail.com>
2021-12-08 10:09:21 +0530

    better login screen :sparkles:

---
+++ App/constants/routes.js
@@ -0,0 +1,19 @@
+export const AUTH = 'Auth';
+export const LOGIN = 'Login';
+export const REGISTER = 'Register';
+export const HOME = 'Home';
+export const DASHBOARD = 'Dashboard';
+export const PROFILE = 'Profile';
+export const MAIN = 'Main';
+export const MODAL = 'MyModal';
+
+export const Routes = {
+  AUTH: 'Auth',
+  LOGIN: 'Login',
+  REGISTER: 'Register',
+  HOME: 'Home',
+  DASHBOARD: 'Dashboard',
+  PROFILE: 'Profile',
+  MAIN: 'Main',
+  MODAL: 'MyModal',
+};
--- App/screens/login/index.js
+++ App/screens/login/index.js
@@ -1,101 +1,158 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
-  Text,
-  TouchableHighlight,
   View,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Platform,
+  Pressable,
 } from 'react-native';
 import {useForm, Controller} from 'react-hook-form';
 import {useDispatch} from 'react-redux';
+import {TextInput, Subheading, Button, HelperText} from 'react-native-paper';
 
-import {Routes} from '../../config';
-import {FormButton, FormInput, ScreenHeader} from '../../components';
-import {useTheme} from '../../contexts';
-import {validators} from '../../utils';
-import {login} from '../../store/slices/authSlice';
+import {Routes} from 'constants/routes';
+import {
+  SizedBox,
+  Container,
+  ViewAreaSafe,
+  ViewAvoidingKeyboard,
+  AuthTitle,
+} from 'components';
+import {validators} from 'utils';
+import {login} from 'store/slices/authSlice';
 
 const LoginScreen = ({navigation}) => {
-  const {handleSubmit, control, errors} = useForm();
-  const dispatch = useDispatch();
   const {
-    theme: {colors},
-  } = useTheme();
+    handleSubmit,
+    control,
+    formState: {errors},
+  } = useForm();
+  const dispatch = useDispatch();
+
+  const emailInput = useRef(null);
+  const passwordInput = useRef(null);
 
   const onSubmit = (data) => {
     // TODO: Present a Loading while logging in. Use global loader context
     dispatch(login({email: data.email, password: data.password}));
   };
 
+  const handleOnSubmit = handleSubmit(onSubmit);
+
+  const navigateToRegister = () => {
+    navigation.navigate(Routes.REGISTER);
+  };
+
   // TODO: Add Server Side Validation
-  //? https://www.carlrippon.com/react-hook-form-server-validation/
-  // TODO: Display Label and Error Message
-  // TODO: Style the Button and Link to other screen
+  // REF: https://www.carlrippon.com/react-hook-form-server-validation/
   return (
-    <>
-      <ScreenHeader title="Student Login" />
-      <SafeAreaView
-        style={[styles.content, {backgroundColor: colors.background}]}>
-        <View style={styles.formView}>
-          <Controller
-            defaultValue=""
-            name="email"
-            control={control}
-            rules={{
-              required: 'E-Mail is required',
-              pattern: {
-                value: validators.EMAIL_REGEX,
-                message: 'Must be a valid email',
-              },
-            }}
-            render={({onChange, onBlur, value}) => (
-              <FormInput
-                placeholder="E-Mail Address"
-                error={errors.email}
-                errorText={errors?.email?.message}
-                onBlur={onBlur}
-                onChangeText={(text) => onChange(text)}
-                value={value}
-                keyboardType="email-address"
-                autoCapitalize="none"
-                autoCorrect={false}
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <Container>
+        <ViewAreaSafe>
+          <ViewAvoidingKeyboard
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+            <AuthTitle>Welcome Back!</AuthTitle>
+
+            <SizedBox height={8} />
+
+            <Subheading>Sign In to your account</Subheading>
+
+            <SizedBox height={32} />
+
+            <Pressable onPress={() => emailInput.current?.focus()}>
+              <Controller
+                name="email"
+                control={control}
+                defaultValue=""
+                rules={{
+                  required: 'E-Mail is required',
+                  pattern: {
+                    value: validators.EMAIL_REGEX,
+                    message: 'Must be a valid email',
+                  },
+                }}
+                render={({onChange, onBlur, value}) => (
+                  <View style={styles.wrapper}>
+                    <TextInput
+                      onBlur={onBlur}
+                      onChangeText={onChange}
+                      value={value}
+                      autoCapitalize="none"
+                      autoCompleteType="email"
+                      autoCorrect={false}
+                      keyboardType="email-address"
+                      ref={emailInput}
+                      returnKeyType="next"
+                      onSubmitEditing={() => passwordInput.current?.focus()}
+                      label="E-Mail"
+                      placeholder="john.doe@gmail.com"
+                      error={!!errors?.email}
+                      style={styles.formText}
+                    />
+
+                    <HelperText type="error" visible={!!errors?.email}>
+                      {errors?.email?.message}
+                    </HelperText>
+                  </View>
+                )}
               />
-            )}
-          />
+            </Pressable>
 
-          <Controller
-            defaultValue=""
-            name="password"
-            control={control}
-            rules={{
-              required: 'Password is required',
-              minLength: {
-                value: 8,
-                message: 'Password must have at least 8 characters',
-              },
-            }}
-            render={({onChange, onBlur, value}) => (
-              <FormInput
-                placeholder="Password"
-                error={errors.password}
-                errorText={errors?.password?.message}
-                onBlur={onBlur}
-                onChangeText={(text) => onChange(text)}
-                value={value}
-                secureTextEntry={true}
+            <SizedBox height={16} />
+
+            <Pressable onPress={() => passwordInput.current?.focus()}>
+              <Controller
+                name="password"
+                control={control}
+                defaultValue=""
+                rules={{
+                  required: 'Password is required',
+                  minLength: {
+                    value: 8,
+                    message: 'Password must have at least 8 characters',
+                  },
+                }}
+                render={({onChange, onBlur, value}) => (
+                  <View style={styles.wrapper}>
+                    <TextInput
+                      onBlur={onBlur}
+                      onChangeText={onChange}
+                      value={value}
+                      autoCapitalize="none"
+                      autoCompleteType="password"
+                      autoCorrect={false}
+                      secureTextEntry
+                      ref={passwordInput}
+                      returnKeyType="done"
+                      onSubmitEditing={handleOnSubmit}
+                      label="Password"
+                      right={<TextInput.Icon name="eye" />} // TODO: add show password on icon click
+                      error={!!errors.password}
+                      style={styles.formText}
+                    />
+
+                    <HelperText type="error" visible={!!errors?.password}>
+                      {errors?.password?.message}
+                    </HelperText>
+                  </View>
+                )}
               />
-            )}
-          />
+            </Pressable>
 
-          <FormButton label="Login" onPress={handleSubmit(onSubmit)} />
+            <SizedBox height={16} />
 
-          <TouchableHighlight
-            onPress={() => navigation.navigate(Routes.REGISTER)}>
-            <Text>Go to Sign-Up Screen</Text>
-          </TouchableHighlight>
-        </View>
-      </SafeAreaView>
-    </>
+            <Button mode="contained" onPress={handleOnSubmit}>
+              Login
+            </Button>
+
+            <SizedBox height={16} />
+
+            <Button onPress={navigateToRegister}>Go to Sign-Up Screen</Button>
+          </ViewAvoidingKeyboard>
+        </ViewAreaSafe>
+      </Container>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -109,6 +166,17 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  formText: {
+    borderRadius: 8,
+    paddingHorizontal: 16,
+  },
+  errorText: {
+    marginTop: 1,
+    color: 'red',
+  },
+  wrapper: {
+    marginBottom: 15,
   },
 });
 
